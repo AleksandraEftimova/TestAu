df43c8bf6f9a8ff911d0589ae37c359f6443c083
Aleksandra <aleftimova@gmail.com>
2023-02-09 19:16:16 +0100

    Au9 - Testing (part 2 - JUNIT)

--- src/main/java/webp/testau/service/implementations/UserServiceImpl.java
+++ src/main/java/webp/testau/service/implementations/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import webp.testau.model.User;
 import webp.testau.model.Role;
 import webp.testau.model.exceptions.InvalidArgumentsException;
+import webp.testau.model.exceptions.InvalidUsernameOrPasswordException;
 import webp.testau.model.exceptions.PasswordDoNotMatchException;
 import webp.testau.model.exceptions.UsernameAlreadyExistsException;
 import webp.testau.repository.jpa.UserRepository;
@@ -28,7 +29,8 @@ public class UserServiceImpl implements UserService {
     public User register(String username, String password, String repeatPassword, String name, String surname, Role role) {
         //proverka
         if (username==null || username.isEmpty() || password==null || password.isEmpty()){
-            throw new InvalidArgumentsException();
+//            throw new InvalidArgumentsException();
+            throw new InvalidUsernameOrPasswordException();
         }
         if (!password.equals(repeatPassword)){
             throw new PasswordDoNotMatchException();
